baee01e9ad15a43bd0045611dbf90043efb0947f
RowellChristian <rcmandac10@gmail.com>
2016-09-11 03:41:15 +0800

    Deleted sound button on login. Added Settings scene with sound on/off.

---
+++ Assets/Scripts/AudioMute.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AudioMute : MonoBehaviour {
+
+	private GameObject theButton;
+	private ColorBlock theColor;
+	bool isMute;
+	bool isClicked = true;
+
+	void Start ()
+	{
+		theButton = GameObject.Find ("btnSound");
+	}
+
+	public void Mute ()
+	{
+		isMute = !isMute;
+			AudioListener.volume =  isMute ? 0 : 1;
+	}
+
+	public void CChange()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour {
+
+	public void SettingsLoad (string scene)
+	{
+		SceneManager.LoadScene (scene);
+	}
+}
