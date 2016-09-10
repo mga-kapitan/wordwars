dd2df5a4becb1a995de95d2cc662a0d43f691c26
RowellChristian <rcmandac>
2016-09-10 18:52:42 +0800

    Fix Scene Load

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -5,42 +5,9 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Login : MonoBehaviour {
-	private string source;
-	private MySqlConnection conexao;
-	private MySqlDataAdapter adapter;
-	private string server;
-	private string database;
-	private string uid;
-	private string password;
 
-	void Start () {
-		source =       "Server=localhost;" +
-			"Database = wordwars;" +    
-			"User ID = root;" +
-			"Pooling = false;" +
-			"Password= ";
-		ConectarBanco(source);
-		Listar (conexao);
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	void ConectarBanco (string _source){
-		conexao = new MySqlConnection (_source);
-		conexao.Open ();
-	}
-
-	void Listar (MySqlConnection _conexao){
-		MySqlCommand comando = _conexao.CreateCommand();
-		comando.CommandText = "Select * from Account";
-		MySqlDataReader dados = comando.ExecuteReader();
-		while (dados.Read()){
-			string nome = (string)dados["nome"];
-			Debug.Log("Usuario " + nome);
-		}
+	public void SceneLoad (string scene)
+	{
+		SceneManager.LoadScene(scene);
 	}
 }
